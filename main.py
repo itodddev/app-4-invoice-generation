3cc5ddf300028f987ac3fca594a991b98d8ed489
Todd James <todd@itodd.co>
2024-03-26 20:10:55 -0400

    Day 26: Add a footer with the total price and the company logo

--- main.py
+++ main.py
@@ -44,5 +44,22 @@ for filepath in filepaths:
         pdf.cell(w=30, h=8, txt=f"{row.price_per_unit}", border=1)
         pdf.cell(w=30, h=8, txt=f"{row.total_price}", border=1, ln=1)
 
+    total_sum = df.total_price.sum()
+    pdf.set_font(family="Times", size=10)
+    pdf.set_text_color(80, 80, 80)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=70, h=8, border=1, align="C")
+    pdf.cell(w=32, h=8, border=1)
+    pdf.cell(w=30, h=8, border=1)
+    pdf.cell(w=30, h=8, txt=f"{total_sum}", border=1, ln=1)
+
+    pdf.ln(3)
+
+    pdf.set_font(family="Times", size=10, style="B")
+    pdf.cell(w=30, h=8, txt=f"The amount due is {total_sum}", ln=1)
+    pdf.set_font(family="Times", size=14, style="B")
+    pdf.cell(w=25, h=8, txt=f"PythonHow")
+    pdf.image("pythonhow.png", w=10,)
+
     pdf.output(f"pdfs/{filename}.pdf")
 
